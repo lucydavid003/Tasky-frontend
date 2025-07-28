6ae15477d7d8105b89f040c21f2b49ff37553aa0
Lucy-Wanjiru <lucywanjiru@gmail.com>
2025-07-28 11:52:19 +0300

    added my url to a global one

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const axiosInstance = axios.create ({
 
-baseURL:"http://127.0.0.1:4000",
+baseURL:"https://tasky-backend-vqjg.onrender.com",
 withCredentials:true
 })
 
