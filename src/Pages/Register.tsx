a912c9f41fa06e920d49ad97a3654d643380d9a9
Lucy-Wanjiru <lucywanjiru@gmail.com>
2025-07-28 12:12:11 +0300

    change axios instance in register.tsx

--- src/Pages/Register.tsx
+++ src/Pages/Register.tsx
@@ -33,10 +33,7 @@ function Register() {
   const { isPending, mutate } = useMutation({
     mutationKey: ["register -user"],
     mutationFn: async (newUser: User) => {
-      const response = await axios.post(
-        "http://127.0.0.1:4000/auth/register",
-        newUser
-      );
+      const response = await axios.post("auth/register",newUser);
 
       return response.data;
     },
